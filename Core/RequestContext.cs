566013038edbea685ae604b1c44cb31f9066668c
Émilien <E1001Ba16@msn.com>
2024-12-13 11:27:35 +0100

    init

---
+++ Core/RequestContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Core;
+
+public interface IRequestContext
+{
+    public string? Tenant { get; set; }
+}
+
+public class RequestContext : IRequestContext
+{
+    public string? Tenant { get; set; }
+}
